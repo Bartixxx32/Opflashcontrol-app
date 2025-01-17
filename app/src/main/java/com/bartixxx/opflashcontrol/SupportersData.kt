e68a44ba029b93aa01e22f9dd82a88cb7d16ed74
Bartixxx32 <mcbplay1@gmail.com>
2025-01-18 00:18:18 +0100

    Upgrade app version to 1.2.1, remove example tests, update kotlin version and add lifecycle-viewmodel dependency (#30)
    Removes example unit and instrumented tests.
    Bumps app versionCode to 24 and versionName to 1.2.1.
    Updates Kotlin API and language version to 2.1.
    Adds androidx.lifecycle:lifecycle-viewmodel dependency with version 2.8.7.
    Updates the supporter avatar link.

--- app/src/main/java/com/bartixxx/opflashcontrol/SupportersData.kt
+++ app/src/main/java/com/bartixxx/opflashcontrol/SupportersData.kt
@@ -4,6 +4,6 @@ data class Supporter(val name: String, val info: String, val avatarUrl: String)
 
 object SupportersData {
     val supporters = listOf(
-        Supporter("Bartixxx", "App Developer", "https://avatars.githubusercontent.com/u/16412925?v=4"),
+        Supporter("Bartixxx", "App Developer", "https://ik.imagekit.io/bartixxx32/github/tr:r-20/u/16412925"),
     )
 }
