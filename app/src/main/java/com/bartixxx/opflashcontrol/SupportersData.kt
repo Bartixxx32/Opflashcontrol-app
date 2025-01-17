6bf92fe23a96badd54de8863830a6b476e872af1
Bartixxx32 <mcbplay1@gmail.com>
2025-01-17 12:00:28 +0100

    Fix duplicate supporter entry in SupportersData

--- app/src/main/java/com/bartixxx/opflashcontrol/SupportersData.kt
+++ app/src/main/java/com/bartixxx/opflashcontrol/SupportersData.kt
@@ -4,8 +4,6 @@ data class Supporter(val name: String, val info: String, val avatarUrl: String)
 
 object SupportersData {
     val supporters = listOf(
-        Supporter("Bartixxx", "Creator", "https://avatars.githubusercontent.com/u/16412925?v=4"),
-        Supporter("Bartixxx", "Creator", "https://avatars.githubusercontent.com/u/16412925?v=4"),
-
+        Supporter("Bartixxx", "App Developer", "https://avatars.githubusercontent.com/u/16412925?v=4"),
     )
 }
