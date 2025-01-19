a6512a3f2e348622cf5a21639b6d4386943fe698
Bartixxx32 <mcbplay1@gmail.com>
2025-01-19 02:52:08 +0100

    Refactor: Add brightness safety, cooldowns, and version bump
    -   Added brightness safety checks to monitor for brightness levels exceeding limits in `MainActivity.kt` and `MainActivity2.kt`, reverting them to safe levels when detected and displaying a message to the user.
    -   Implemented cooldown to limit the use of the eye destroyer functionality.
    -   Bumped `versionCode` to 26 and `versionName` to 1.2.3 in `build.gradle.kts`.
    -   Added `noGCSettingWarning` to the `BuildAttributionWarningsFilter` in `.idea/misc.xml`.
    - Added `safety` string in `strings.xml` and `strings.xml`(PL).

--- app/src/main/java/com/bartixxx/opflashcontrol/MainActivity.kt
+++ app/src/main/java/com/bartixxx/opflashcontrol/MainActivity.kt
@@ -6,18 +6,37 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import com.bartixxx.opflashcontrol.MainActivity2
 import com.bartixxx.opflashcontrol.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     private var clickCount = 0
     private var lastClickTime: Long = 0
+    private var brightnessCheckHandler: Handler? = null
+    private var brightnessCheckRunnable: Runnable? = null
+    private var brightnessExceededTime: Long = 0
+    private var safetyTriggered = false
+    private val MAX_BRIGHTNESS = 150
+    private val SAFE_BRIGHTNESS = 80
+    private val CHECK_INTERVAL = 200L // 200 ms
+    private var eyeDestroyerCooldown = false // Flag to track cooldown
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize the brightness check handler
+        brightnessCheckHandler = Handler(Looper.getMainLooper())
+        brightnessCheckRunnable = Runnable {
+            checkBrightnessSafety()
+            brightnessCheckHandler?.postDelayed(brightnessCheckRunnable!!, CHECK_INTERVAL)
+        }
+        brightnessCheckHandler?.postDelayed(brightnessCheckRunnable!!, CHECK_INTERVAL)
 
         // Set the range of the sliders
         with(binding) {
@@ -32,23 +51,29 @@ class MainActivity : BaseActivity() {
             yellowSeekBar.valueTo = 255f
 
             setupSlider(masterSeekBar, masterTextView, "Master Brightness") { progress ->
-                masterBrightness = progress
-                if (isLedOn && whiteBrightness <= 1 && yellowBrightness <= 1) {
-                    controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH, whiteBrightness = progress, yellowBrightness = progress)
+                if (!safetyTriggered) {
+                    masterBrightness = progress
+                    if (isLedOn && whiteBrightness <= 1 && yellowBrightness <= 1) {
+                        controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH, whiteBrightness = progress, yellowBrightness = progress)
+                    }
                 }
             }
 
             setupSlider(whiteSeekBar, whiteTextView, "White Brightness") { progress ->
-                whiteBrightness = progress
-                if (isLedOn) {
-                    controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH, whiteBrightness = progress, yellowBrightness = yellowBrightness)
+                if (!safetyTriggered) {
+                    whiteBrightness = progress
+                    if (isLedOn) {
+                        controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH, whiteBrightness = progress, yellowBrightness = yellowBrightness)
+                    }
                 }
             }
 
             setupSlider(yellowSeekBar, yellowTextView, "Yellow Brightness") { progress ->
-                yellowBrightness = progress
-                if (isLedOn) {
-                    controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH, whiteBrightness = whiteBrightness, yellowBrightness = progress)
+                if (!safetyTriggered) {
+                    yellowBrightness = progress
+                    if (isLedOn) {
+                        controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH, whiteBrightness = whiteBrightness, yellowBrightness = progress)
+                    }
                 }
             }
 
@@ -68,11 +93,11 @@ class MainActivity : BaseActivity() {
                 VibrationUtil.vibrate(this@MainActivity, 100L)
                 navigateToMainActivity2()
             }
-
             // Set up click listener for the title which requires 5 clicks within 5 seconds
             flashbrightness.setOnClickListener {
                 val currentTime = System.currentTimeMillis()
                 VibrationUtil.vibrate(this@MainActivity, 100L)
+
                 if (currentTime - lastClickTime > 5000) {
                     clickCount = 1
                 } else {
@@ -144,15 +169,78 @@ class MainActivity : BaseActivity() {
     }
 
     private fun executeExtraFunction() {
+        if (eyeDestroyerCooldown) {
+            Toast.makeText(this, "Please wait before using this feature again.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Perform the eye destroyer functionality
         controlLeds("off", FLASH_WHITE_LED_PATH, FLASH_YELLOW_LED_PATH, whiteBrightness = 1000, yellowBrightness = 1000)
         controlLeds("on", FLASH_WHITE_LED_PATH, FLASH_YELLOW_LED_PATH, whiteBrightness = 1500, yellowBrightness = 1500)
         isLedOn = true
+
+        // Start cooldown
+        startEyeDestroyerCooldown()
+    }
+    private fun startEyeDestroyerCooldown() {
+        eyeDestroyerCooldown = true
+        binding.destroyer.isEnabled = false // Disable the button
+
+        // Re-enable after 5 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            eyeDestroyerCooldown = false
+            binding.destroyer.isEnabled = true // Enable the button
+        }, 5000) // 5 seconds delay
     }
 
     private fun navigateToMainActivity2() {
         startActivity(Intent(this, MainActivity2::class.java))
     }
 
+    private fun checkBrightnessSafety() {
+        val currentTime = System.currentTimeMillis()
+
+        // Check if any brightness exceeds the limit
+        if (masterBrightness > MAX_BRIGHTNESS || whiteBrightness > MAX_BRIGHTNESS || yellowBrightness > MAX_BRIGHTNESS) {
+            if (brightnessExceededTime == 0L) {
+                brightnessExceededTime = currentTime
+            } else if (currentTime - brightnessExceededTime > 20000) { // More than 2 seconds
+                revertExceedingBrightnessToSafeLevel()
+            }
+        } else {
+            brightnessExceededTime = 0L // Reset the timer if brightness is within safe range
+            safetyTriggered = false // Allow user to adjust brightness again
+        }
+    }
+
+    private fun revertExceedingBrightnessToSafeLevel() {
+        // Check and revert only the brightness value that exceeds the limit
+        if (masterBrightness > MAX_BRIGHTNESS) {
+            masterBrightness = SAFE_BRIGHTNESS
+            binding.masterSeekBar.value = SAFE_BRIGHTNESS.toFloat()
+        }
+
+        if (whiteBrightness > MAX_BRIGHTNESS) {
+            whiteBrightness = SAFE_BRIGHTNESS
+            binding.whiteSeekBar.value = SAFE_BRIGHTNESS.toFloat()
+        }
+
+        if (yellowBrightness > MAX_BRIGHTNESS) {
+            yellowBrightness = SAFE_BRIGHTNESS
+            binding.yellowSeekBar.value = SAFE_BRIGHTNESS.toFloat()
+        }
+
+        // Apply the changes to the LEDs
+        if (isLedOn) {
+            controlLeds("on", WHITE_LED_PATH, YELLOW_LED_PATH,
+                whiteBrightness = if (whiteBrightness > MAX_BRIGHTNESS) SAFE_BRIGHTNESS else whiteBrightness,
+                yellowBrightness = if (yellowBrightness > MAX_BRIGHTNESS) SAFE_BRIGHTNESS else yellowBrightness
+            )
+        }
+
+        safetyTriggered = true
+        Toast.makeText(this, "Brightness exceeded limit! Adjusted to safe levels.", Toast.LENGTH_SHORT).show()
+    }
     private fun performSecretAction() {
         VibrationUtil.vibrate(this, 200L)
         Toast.makeText(this, getString(R.string.experimental), Toast.LENGTH_SHORT).show()
