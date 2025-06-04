fb3570a17f17fbe2d1c8f845bcc8e8d5ffa9c3ee
Your Name <you@example.com>
2025-06-04 05:35:01 +0200

    Bump com.android.application from 8.10.0 to 8.10.1
    Bumps com.android.application from 8.10.0 to 8.10.1.
    Also increments app version code to 40 and version name to "1.3.13".
    
    ---
    updated-dependencies:
    - dependency-name: com.android.application
      dependency-version: 8.10.1
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 31
         targetSdk = 35
 
-        versionCode = 39
-        versionName = "1.3.12"
+        versionCode = 40
+        versionName = "1.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
